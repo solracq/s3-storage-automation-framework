d5571c2cd90ddac17e187d379daf7a8de907804c
Carlos Quiroz <solracq@gmail.com>
2026-05-11 16:35:55 -0400

    Added logging messages to Client and Resource classes

--- src/storage_automation/s3_client.py
+++ src/storage_automation/s3_client.py
@@ -3,7 +3,9 @@ from src.storage_automation.interfaces.s3 import S3
 from botocore.client import Config
 from botocore.exceptions import ClientError
 from app.storage_api.settings import Settings
+import logging
 
+logger = logging.getLogger(__name__)
 
 class S3Client(S3):
 
@@ -27,6 +29,7 @@ class S3Client(S3):
             self.s3_client.head_bucket(Bucket=bucket_name)
             return True
         except ClientError as error:
+            logger.error(f"Bucket does not exist: {bucket_name}")
             return False
 
     def create_bucket(self, bucket_name: str) -> dict:
@@ -34,6 +37,7 @@ class S3Client(S3):
             self.s3_client.create_bucket(Bucket=bucket_name)
             return {"message": "Bucket created successfully"}
         except ClientError as error:
+            logger.error(f"Bucket already exists: {bucket_name}")
             return {"message": "Bucket already exists"}
 
     def delete_bucket(self, bucket_name: str) -> dict:
@@ -41,30 +45,35 @@ class S3Client(S3):
             self.s3_client.delete_bucket(Bucket=bucket_name)
             return {"message": "Bucket deleted successfully"}
         except ClientError as error:
+            logger.error(f"Bucket deletion failed: {bucket_name}")
             return {"message": "Bucket deletion failed"}
 
     def list_buckets(self) -> dict:
         try:
             return self.s3_client.list_buckets()
         except ClientError as error:
+            logger.error(f"Bucket listing failed: {error}")
             return {"message": "Bucket listing failed"}
 
     def list_objects(self, bucket_name: str) -> dict:
         try:
             return self.s3_client.list_objects(Bucket=bucket_name)
         except ClientError as error:
+            logger.error(f"Objects listing failed: {error}")
             return {"message": "Objects listing failed"}
 
     def get_object_metadata(self, bucket_name: str, object_key: str) -> dict:
         try:
             return self.s3_client.head_object(Bucket=bucket_name, Key=object_key)
         except ClientError as error:
+            logger.error(f"Object metadata retrieval failed: {error}")
             return {"message": "Object metadata retrieval failed"}
 
     def get_object_size(self, bucket_name: str, object_key: str) -> dict:
         try:
             return self.s3_client.head_object(Bucket=bucket_name, Key=object_key)["ContentLength"]
         except ClientError as error:
+            logger.error(f"Object size retrieval failed: {error}")
             return {"message": "Object size retrieval failed"}
 
     def write_object(self, bucket_name: str, object_key: str, content: bytes) -> dict:
@@ -72,12 +81,14 @@ class S3Client(S3):
             self.s3_client.put_object(Bucket=bucket_name, Key=object_key, Body=content)
             return {"message": "Object written successfully"}
         except ClientError as error:
+            logger.error(f"Object writing failed: {error}")
             return {"message": "Object writing failed"}
 
     def read_object(self, bucket_name: str, object_key: str) -> dict:
         try:
             return self.s3_client.get_object(Bucket=bucket_name, Key=object_key)["Body"].read()
         except ClientError as error:
+            logger.error(f"Object reading failed: {error}")
             return {"message": "Object reading failed"}
 
     def delete_object(self, bucket_name: str, object_key: str) -> dict:
@@ -85,6 +96,7 @@ class S3Client(S3):
             self.s3_client.delete_object(Bucket=bucket_name, Key=object_key)
             return {"message": "Object deleted successfully"}
         except ClientError as error:
+            logger.error(f"Object deletion failed: {error}")
             return {"message": "Object deletion failed"}
 
     def upload_object(self, bucket_name: str, object_key: str, file_path: str) -> dict:
@@ -92,6 +104,7 @@ class S3Client(S3):
             self.s3_client.upload_file(file_path, bucket_name, object_key)
             return {"message": "Object uploaded successfully"}
         except ClientError as error:
+            logger.error(f"Object upload failed: {error}")
             return {"message": "Object upload failed"}
 
     def download_object(self, bucket_name: str, object_key: str, file_path: str) -> dict:
@@ -99,4 +112,5 @@ class S3Client(S3):
             self.s3_client.download_file(bucket_name, object_key, file_path)
             return {"message": "Object downloaded successfully"}
         except ClientError as error:
+            logger.error(f"Object download failed: {error}")
             return {"message": "Object download failed"}
--- src/storage_automation/s3_resource.py
+++ src/storage_automation/s3_resource.py
@@ -3,6 +3,9 @@ from botocore.client import Config
 from botocore.exceptions import ClientError
 from src.storage_automation.interfaces.s3 import S3
 from app.storage_api.settings import Settings
+import logging
+
+logger = logging.getLogger(__name__)
 
 class S3Resource(S3):
     def __init__(self) -> None:
@@ -26,6 +29,7 @@ class S3Resource(S3):
             self.s3_resource.Bucket(bucket_name).exists()
             return True
         except ClientError as error:
+            logger.error(f"Bucket does not exist: {bucket_name}")
             return False
 
     def create_bucket(self, bucket_name: str) -> dict:
@@ -33,6 +37,7 @@ class S3Resource(S3):
             self.s3_resource.create_bucket(Bucket=self.bucket)
             return {"message": "Bucket created successfully"}
         except ClientError as error:
+            logger.error(f"Bucket creation failed: {error}")
             return {"message": "Bucket creation failed"}
 
     def delete_bucket(self, bucket_name: str) -> dict:
@@ -40,12 +45,14 @@ class S3Resource(S3):
             self.s3_resource.delete_bucket(Bucket=self.bucket)
             return {"message": "Bucket deleted successfully"}
         except ClientError as error:
+            logger.error(f"Bucket deletion failed: {error}")
             return {"message": "Bucket deletion failed"}
 
     def list_buckets(self) -> dict:
         try:
             return self.s3_resource.buckets.all()
         except ClientError as error:
+            logger.error(f"Bucket listing failed: {error}")
             return {"message": "Bucket listing failed"}
 
     def list_objects(self, bucket_name: str) -> dict:
@@ -53,18 +60,21 @@ class S3Resource(S3):
             return self.bucket.objects.all()
             return {"message": "Objects listed successfully"}
         except ClientError as error:
+            logger.error(f"Objects listing failed: {error}")
             return {"message": "Objects listing failed"}   
 
     def get_object_metadata(self, bucket_name: str, object_key: str) -> dict:
         try:
             return self.bucket.objects.filter(Prefix=object_key).first().metadata
         except ClientError as error:
+            logger.error(f"Object metadata retrieval failed: {error}")
             return {"message": "Object metadata retrieval failed"}
 
     def get_object_size(self, bucket_name: str, object_key: str) -> dict:
         try:
             return self.bucket.objects.filter(Prefix=object_key).first().size
         except ClientError as error:
+            logger.error(f"Object size retrieval failed: {error}")
             return {"message": "Object size retrieval failed"}
 
     def write_object(self, bucket_name: str, object_key: str, content: bytes) -> dict:
@@ -72,12 +82,14 @@ class S3Resource(S3):
             self.bucket.objects.filter(Prefix=object_key).write(content)
             return {"message": "Object written successfully"}
         except ClientError as error:
+            logger.error(f"Object writing failed: {error}")
             return {"message": "Object writing failed"}
 
     def read_object(self, bucket_name: str, object_key: str) -> dict:
         try:
             return self.bucket.objects.filter(Prefix=object_key).first().read()
         except ClientError as error:
+            logger.error(f"Object reading failed: {error}")
             return {"message": "Object reading failed"}
 
     def delete_object(self, bucket_name: str, object_key: str) -> dict:
@@ -85,6 +97,7 @@ class S3Resource(S3):
             self.bucket.objects.filter(Prefix=object_key).delete()
             return {"message": "Object deleted successfully"}
         except ClientError as error:
+            logger.error(f"Object deletion failed: {error}")
             return {"message": "Object deletion failed"}
 
     def upload_object(self, bucket_name: str, object_key: str, file_path: str) -> dict:
@@ -92,6 +105,7 @@ class S3Resource(S3):
             self.bucket.upload_file(file_path, object_key)
             return {"message": "Object uploaded successfully"}
         except ClientError as error:
+            logger.error(f"Object upload failed: {error}")
             return {"message": "Object upload failed"}
 
     def download_object(self, bucket_name: str, object_key: str, file_path: str) -> dict:
@@ -99,4 +113,5 @@ class S3Resource(S3):
             self.bucket.download_file(object_key, file_path)
             return {"message": "Object downloaded successfully"}
         except ClientError as error:
+            logger.error(f"Object download failed: {error}")
             return {"message": "Object download failed"}
