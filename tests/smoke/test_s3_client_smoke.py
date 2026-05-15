4dbf524bd55c56a878e1acdf647acacc45a249d0
Carlos Quiroz <solracq@gmail.com>
2026-05-14 20:48:33 -0400

    Added initial automated test suite for smoke scenarios

---
+++ tests/smoke/test_s3_client_smoke.py
@@ -0,0 +1,35 @@
+import pytest
+
+from src.storage_automation import s3_client as s3_client_module
+from src.storage_automation.s3_client import S3Client
+from tests.utils.constants import BASE_URL, BUCKET, SECONDARY_BUCKET, OBJECT_KEY
+
+
+pytestmark = pytest.mark.smoke
+
+# Fixtures
+
+@pytest.fixture
+def s3_client(monkeypatch):
+    monkeypatch.setattr(s3_client_module.settings, "s3_endpoint_url", BASE_URL)
+    monkeypatch.setattr(s3_client_module.settings, "aws_access_key_id", "minioadmin")
+    monkeypatch.setattr(s3_client_module.settings, "aws_secret_access_key", "minioadmin123")
+    return S3Client()
+
+@pytest.fixture
+def initialize_bucket(s3_client):
+    s3_client.create_bucket(BUCKET)
+    yield
+    s3_client.delete_object(BUCKET, OBJECT_KEY)
+    s3_client.delete_bucket(BUCKET)
+
+
+# Tests
+
+class TestS3ClientSmoke:
+    def test_list_buckets(self, s3_client, initialize_bucket):
+        response = s3_client.list_buckets()
+        
+        assert isinstance(response, dict), "Response is not of dictionary type"
+        assert "Buckets" in response, "'Buckets' string missing in response"
+        assert any(bucket["Name"] == BUCKET for bucket in response["Buckets"]), f"Bucket name '{BUCKET}' is missing in response"
