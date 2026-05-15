0dfbf07da7b9476a64ebab77a9cb0d0eac0e19a1
Carlos Quiroz <solracq@gmail.com>
2026-05-15 11:49:45 -0400

    Updated from absolute to a relative path in a constant variable and moved supporting data files on a more logical location

--- tests/utils/constants.py
+++ tests/utils/constants.py
@@ -1,9 +1,15 @@
 import os
+from pathlib import Path
+
 from app.storage_api.settings import settings
+
+ROOT_DIR = Path(__file__).resolve().parents[2]
+TEST_ASSETS_DIR = ROOT_DIR / "docs" / "Postman_collection_run_files"
 
 BUCKET = settings.minio_bucket_name
 SECONDARY_BUCKET = "test-bucket2"
 OBJECT_KEY = "sample.txt"
 BASE_URL = os.getenv("S3_PUBLIC_ENDPOINT_URL", "http://127.0.0.1:9000")
-CONTENT_DATA = "Hello from the write_object endpoint"
-FILE_PATH = "/Users/carlosquiroz/dev/s3-storage-automation-framework/docs/Postman_collection_run_files/sample.txt"
+CONTENT_DATA = b"Hello from the write_object endpoint"
+FILE_PATH = TEST_ASSETS_DIR / "sample.txt"
+
