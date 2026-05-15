c987070d692f8219d49c5e056a745517c693b951
Carlos Quiroz <solracq@gmail.com>
2026-05-14 21:18:35 -0400

    Added two new smoke tests about creation anddeltion of bucket

--- tests/smoke/test_s3_client_smoke.py
+++ tests/smoke/test_s3_client_smoke.py
@@ -7,6 +7,7 @@ from tests.utils.constants import BASE_URL, BUCKET, SECONDARY_BUCKET, OBJECT_KEY
 
 pytestmark = pytest.mark.smoke
 
+
 # Fixtures
 
 @pytest.fixture
@@ -15,6 +16,7 @@ def s3_client(monkeypatch):
     monkeypatch.setattr(s3_client_module.settings, "aws_access_key_id", "minioadmin")
     monkeypatch.setattr(s3_client_module.settings, "aws_secret_access_key", "minioadmin123")
     return S3Client()
+
 
 @pytest.fixture
 def initialize_bucket(s3_client):
@@ -27,9 +29,27 @@ def initialize_bucket(s3_client):
 # Tests
 
 class TestS3ClientSmoke:
+    def test_create_bucket(self, s3_client):
+        response = s3_client.create_bucket(BUCKET)
+
+        assert isinstance(response, dict), "Response must be of dictionary type"
+        assert response['message'] == "Bucket created successfully", "No successful bucket creation in response"
+        
+        s3_client.delete_bucket(BUCKET)
+
+
+    def test_delete_bucket(self, s3_client):
+        s3_client.create_bucket(BUCKET)
+
+        response = s3_client.delete_bucket(BUCKET)
+
+        assert isinstance(response, dict), "Response must be of dictionary type"
+        assert response['message'] == "Bucket deleted successfully", "No successful bucket deletion in response"
+
+
     def test_list_buckets(self, s3_client, initialize_bucket):
         response = s3_client.list_buckets()
         
-        assert isinstance(response, dict), "Response is not of dictionary type"
+        assert isinstance(response, dict), "Response must be of dictionary type"
         assert "Buckets" in response, "'Buckets' string missing in response"
         assert any(bucket["Name"] == BUCKET for bucket in response["Buckets"]), f"Bucket name '{BUCKET}' is missing in response"
